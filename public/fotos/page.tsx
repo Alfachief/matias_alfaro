5e78c0e8de69b76b27c143edfb20a3a395da0642
Lucas <lucasalfaromedina@gmail.com>
2026-08-16 21:06:14 -0300

    Corregir ubicación de archivos de galería

--- public/fotos/page.tsx
+++
@@ -1,9 +0,0 @@
-import PhotoGallery from "../../../components/PhotoGallery";
-
-export default function FotosPage() {
-  return (
-    <div style={{ background: "#141210", minHeight: "100vh" }}>
-      <PhotoGallery />
-    </div>
-  );
-}
